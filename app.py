fcac3b097711155e1cde03ff1bce62a0c77e78f5
joncj21-sys <joncj21@gmail.com>
2026-04-09 12:49:57 -0600

    Change Flask app run configuration
    Updated the app to run on host 0.0.0.0 and port 10000.

--- app.py
+++ app.py
@@ -2,30 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Lista de películas (puedes cambiar los links)
 peliculas = [
     {
         "titulo": "Acción Extrema",
-        "url": "https://www.youtube.com/watch?v=DFs7Vnautps&list=RDGMEM0s70dY0AfCwh3LqQ-Bv1xg&index=18"
+        "url": "https://www.youtube.com/watch?v=DFs7Vnautps"
     },
     {
         "titulo": "Comedia Divertida",
         "url": "https://www.w3schools.com/html/movie.mp4"
-    }
-    ,
+    },
     {
-        "titulo":"extrem",
+        "titulo": "Extrem",
         "url": "/static/videos/0408(1).mp4"
     }
-
-
 ]
 
 @app.route("/")
 def inicio():
     return render_template("index.html", peliculas=peliculas)
 
+# IMPORTANTE PARA RENDER
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host="0.0.0.0", port=10000)
 
